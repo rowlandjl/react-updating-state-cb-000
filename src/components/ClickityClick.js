edc24bd50e380061c7b23664f6231113d1804b0c
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-18 00:47:14 +0000

    Automatically backed up by Learn

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -12,8 +12,7 @@ class ClickityClick extends React.Component {
   handleClick = () => {
     this.setState({
       hasBeenClicked: true
-    })
-    console.log(this.state.hasBeenClicked);
+    }, () => console.log(this.state.hasBeenClicked))
   };
   
   render() {
