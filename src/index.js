1806ea2c9a9fe50c2522df222a69350ddde97fd6
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-18 00:22:11 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ClickityClick from './components/ClickityClick';
 
 ReactDOM.render(
   <div>
-    Mount Components Here
+    <ClickityClick />,
   </div>,
   document.getElementById('root')
 );
